cc964f060bfa422b996b384137605d2b93f52698
daejung83 <daejung1@gmail.com>
2017-01-25 22:48:06 -0600

    Turning in my activity

--- activities/a02-hydra/src/Lib.hs
+++ activities/a02-hydra/src/Lib.hs
@@ -9,7 +9,19 @@ module Lib
 -- chop :: [Int] -> [Int]
 
 chop :: [Int] -> [Int]
-chop = undefined
+--chop [] = []
+--chop n = n
+
+chop [] = []
+chop n = aux n ((length n) - 1)
+	where aux (x:xs) loc = do
+		if xs == [] && x > 0
+			then (x-1):[]	
+			else if x > 0
+				then (x-1):head(xs)+loc:tail(xs)
+				else x:chop xs
+			 
+	
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
