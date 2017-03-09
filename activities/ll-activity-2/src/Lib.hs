296fc1af75b3e9a72c991021751d067359b5042a
daejung83 <daejung1@gmail.com>
2017-03-08 21:14:51 -0600

    activity commit

--- activities/ll-activity-2/src/Lib.hs
+++ activities/ll-activity-2/src/Lib.hs
@@ -41,7 +41,38 @@ parseSymbol s (x:xs) =
 --    | ( E )
 --    | let var = E in E end
 
-
 parse xx = parseE (words xx)
 
-parseE = undefined
+parseE ("+":xs) =
+  let (e1,r1) = parseE xs
+      (e2,r2) = parseE r1
+   in (PlusExp e1 e2, r2)
+
+--parseE ("(":xs) = parseE xs
+    --let (e1, r1) = parseE (xs)
+    --    in parseE r1
+
+parseE ("let":xs) =
+    let (e1,r1) = parseE xs
+        (_, rr1) = parseSymbol "=" r1
+        (e2,r2) = parseE rr1
+        (_, rr2) = parseSymbol "in" r2
+        (e3,r3) = parseE rr2
+        (_, rr3) = parseSymbol "end" r3
+        in ((LetExp (display e1) e2 e3), rr3)
+
+--parseE ("end":xs) = parseE xs
+--parseE ("in":xs) = parseE xs
+
+parseE (x:xs) | isInt x =
+                (IntExp (read x), xs)
+              | isSymbol "(" x = 
+                    let (e1, r1) = parseE xs
+                        (e2, rr1) = (parseSymbol ")" r1)
+                    in (e1, rr1)
+              | otherwise =
+                (VarExp x, xs)  
+
+
+
+
